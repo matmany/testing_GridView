aeaaa5e12d451e7d25c6c33449ec13556205928a
mateusmamani <matmanymat@gmail.com>
2016-07-20 15:59:48 -0300

    -Implementar GridView com layouts

--- app/src/main/java/com/example_frag2/mamani/testing_gridview/Adaptador.java
+++ app/src/main/java/com/example_frag2/mamani/testing_gridview/Adaptador.java
@@ -1,27 +1,34 @@
 package com.example_frag2.mamani.testing_gridview;
 
 import android.content.Context;
+import android.os.Build;
+import android.support.percent.PercentRelativeLayout;
+import android.text.Layout;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 
 /**
  * Created by mamani on 18/07/16.
  */
 public class Adaptador extends BaseAdapter {
     private Context context;
-    private int img;
+    private int[] lista;
 
-    public Adaptador(Context ctx,int image)
+
+    public Adaptador(Context ctx,int[] lista)
     {
         this.context = ctx;
-        this.img=image;
+        this.lista=lista;
+
     }
 
 
     @Override
     public int getCount() {
-        return 1;
+        return lista.length;
     }
 
     @Override
@@ -36,6 +43,29 @@ public class Adaptador extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+
+        ImageView iv = new ImageView(context);
+
+        PercentRelativeLayout percentRelativeLayout = new PercentRelativeLayout(context);
+
+        ViewGroup viewGroup1 = new ViewGroup(context) {
+            @Override
+            protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+            }
+        };
+
+
+        //iv.setLayoutParams(new GridView.LayoutParams(150,100));
+       // iv.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        //iv.setPadding(4,4,4,4);
+
+
+        iv.setImageResource(lista[i]);
+
+
+        iv.setAdjustViewBounds(true); // ajusta o taamanho da image de acordo com a coluna
+
+        return iv;
     }
 }
--- app/src/main/java/com/example_frag2/mamani/testing_gridview/MainActivity.java
+++ app/src/main/java/com/example_frag2/mamani/testing_gridview/MainActivity.java
@@ -2,18 +2,49 @@ package com.example_frag2.mamani.testing_gridview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterViewAnimator;
 import android.widget.GridView;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
+         //int image = R.drawable.gsa_chair_empty;
+         int image = R.layout.caderno_image;
 
-        int image = R.drawable.gsa_chair_empty;
+        int[] lista = new int[]{
+                image,image,image,image,image,image,image,image,image,
+                image,image,image,image,image,image,image,image,image,
+                image,image,image,image,image,image,image,image,image,
+                image,image,image,image,image,image,image,image,image,
+                image,image,image,image,image,image,image,image,image,
+                image,image,image,image,image,image,image,image,image,
+                image,image,image,image,image,image,image,image,image,
+                image,image,image,image,image,image,image,image,image,
+                image,image,image,image,image,image,image,image,image,
+                image,image,image,image,image,image,image,image,image,
+                };
+
+
         GridView gr = (GridView) findViewById(R.id.gridView);
+
+        gr.setAdapter(new Adaptador(this,lista));
+
+        gr.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this,""+i,Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
 
